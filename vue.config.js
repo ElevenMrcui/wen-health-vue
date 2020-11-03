75152e89e8bf187851a6bc924e3cc81af8ebc623
唐彭 <tps520tps@163.com>
2020-11-03 17:19:59 +0800

    update vue.config.js.

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     port:8020,
-    proxy: {   //  解决 跨域问题
+    proxy: {   //  解决 跨域问题   配置后台服务器地址
       '/api': {
         target: 'http://localhost:8081',
         changeOrigin: true,
