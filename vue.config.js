c662c8c6deb5512d06c8e0ea6287500c971651a7
tps52 <tps520tps@163.com>
2020-11-03 16:58:01 +0800

    day01-传智健康前端-项目初始化

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   productionSourceMap: false,
   lintOnSave: false,
   devServer: {
-    port:8888,
-    proxy: {
+    port:8020,
+    proxy: {   //  解决 跨域问题
       '/api': {
         target: 'http://localhost:8081',
         changeOrigin: true,
